770365efbb6cbb9d5cda6c7ba97cc7981557f8d1
chionsuk <hsuk0409@gmail.com>
2022-04-30 23:19:24 +0900

    패키지 이름 변경

--- wando/src/main/java/com/eatthis/eatthis/EatThisApplication.java
+++ wando/src/main/java/com/eatthis/eatthis/EatThisApplication.java
@@ -1,4 +1,4 @@
-package com.eatthos.eatthis;
+package com.eatthis.eatthis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
