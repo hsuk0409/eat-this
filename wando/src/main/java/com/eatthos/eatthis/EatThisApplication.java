cf1fd28e4a9004d0f077c7b6257e9bb017f81dfb
chionsuk <hsuk0409@gmail.com>
2022-04-30 22:16:46 +0900

    두번째 프로젝트 첫 커밋

---
+++ wando/src/main/java/com/eatthos/eatthis/EatThisApplication.java
@@ -0,0 +1,13 @@
+package com.eatthos.eatthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatThisApplication.class, args);
+	}
+
+}
