6ae04617eb12390e6fc8c213a38c35190d0f89de
chionsuk <hsuk0409@gmail.com>
2022-05-03 21:47:46 +0900

    헬스첵 컨트롤러 생성

---
+++ wando/src/main/java/com/eatthis/eatthis/web/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.eatthis.eatthis.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthCheck")
+public class HealthCheckController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "Health Check OK!!";
+    }
+
+}
