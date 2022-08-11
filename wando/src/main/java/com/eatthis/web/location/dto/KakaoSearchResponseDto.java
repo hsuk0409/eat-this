76c62b5d24165c92bba9f020d58dedfd10d36629
chionsuk <hsuk0409@gmail.com>
2022-08-11 22:17:43 +0900

    조회 후 주소에 대한 음면동 추출 로직 전

--- wando/src/main/java/com/eatthis/web/location/dto/KakaoSearchResponseDto.java
+++ wando/src/main/java/com/eatthis/web/location/dto/KakaoSearchResponseDto.java
@@ -4,6 +4,7 @@ import com.eatthis.web.location.domain.LocationCategory;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.ObjectUtils;
 
 @Getter
 @NoArgsConstructor
@@ -13,6 +14,7 @@ public class KakaoSearchResponseDto {
     private String placeName;
     private String address;
     private String roadAddress;
+    private String eupMyeonDong;
     private String lng; // x, 경도
     private String lat; // y, 위도
     private String distance; // x,y 존재할 경우 중심 좌표까지의 거리
@@ -26,11 +28,22 @@ public class KakaoSearchResponseDto {
         this.placeName = placeName;
         this.address = address;
         this.roadAddress = roadAddress;
+        this.eupMyeonDong = setEupMyeonDongByAddress(address);
         this.lng = lng;
         this.lat = lat;
         this.distance = distance;
         this.phoneNumber = phoneNumber;
         this.category = category;
+    }
+
+    private String setEupMyeonDongByAddress(String address) {
+        if (ObjectUtils.isEmpty(address)) {
+            return "내주변"; // 혹시나 주소 없을 경우 기존 텍스트 반환
+        }
+
+        // TODO Biz logic
+        
+        return null;
     }
 
 }
