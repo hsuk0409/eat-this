53386b2c54ca052364d8e8cf8fa9335995dff3f3
chionsuk <hsuk0409@gmail.com>
2022-12-12 00:25:04 +0900

    fcm 테스트 케이스

--- wando/src/test/java/com/eatthis/api/firebase/FirebaseMessageServiceTest.java
+++ wando/src/test/java/com/eatthis/api/firebase/FirebaseMessageServiceTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.eatthis.api.firebase;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
 class FirebaseMessageServiceTest {
-  
+
+    @Autowired
+    private FirebaseMessageService firebaseMessageService;
+
+    @DisplayName("임의의 fcm 토큰으로 푸시 알림을 성공적으로 보낸다.")
+    @Test
+    void sendFcmMessage() throws IOException {
+        //given
+        String fcmToken = "";
+        String title = "";
+        String contents = "";
+
+        //when & then
+        firebaseMessageService.sendMessageTo(fcmToken, title, contents);
+    }
+
 }
